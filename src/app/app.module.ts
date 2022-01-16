8308a247a3afc3787e74c39a8174ef7319a36e14
rajeshkethan <rajeshkethan@gmail.com>
2022-01-16 17:41:55 +0530

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './profile/login/login.component';
+import { SignupComponent } from './profile/signup/signup.component';
+import { ForgotPasswordComponent } from './profile/forgot-password/forgot-password.component';
+import { HeaderComponent } from './header/header/header.component';
+import { FooterComponent } from './header/footer/footer.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    SignupComponent,
+    ForgotPasswordComponent,
+    HeaderComponent,
+    FooterComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
